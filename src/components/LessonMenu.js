7c044e6297368490aa68fb9407a36731bde9f4ca
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-06-18 00:39:35 +0100

    Lesson menu styling

--- src/components/LessonMenu.js
+++ src/components/LessonMenu.js
@@ -9,10 +9,12 @@ var LessonMenu = React.createClass({
     return <div className="LessonMenu">
       {lessons.map((lesson, index) => {
         var isActive = index === currentLessonIndex
-        return <a className={classNames({'active': isActive})}
+        return <a className={classNames('LessonMenu__lesson', {
+                    'LessonMenu__lesson--active': isActive
+                  })}
                   onClick={!isActive && this.props.selectLesson.bind(null, index)}>
           <div>
-            <span className="number">{index + 1}</span>
+            <span className="LessonMenu__number">{index + 1}</span>
           </div>
         </a>
       })}
