a55504fc49b383b4f986d8253ea98544aba4c944
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-06-17 13:21:28 +0100

    Improved styling when editing the lesson name

--- src/components/LessonHeader.js
+++ src/components/LessonHeader.js
@@ -8,11 +8,13 @@ var LessonHeader = React.createClass({
     var {addStep, currentStepIndex, editing, lesson, lessonNumber, selectStep, updateLesson} = this.props
     return <div className="LessonHeader">
       <h2>
-        {`${lessonNumber}. `}
-        {editing
-         ? <input value={lesson.name} onChange={e => updateLesson({name: e.target.value})}/>
-         : lesson.name
-        }
+        <span className="LessonHeader__number">{lessonNumber}.</span>
+        <span className="LessonHeader__name">
+          {editing
+           ? <input value={lesson.name} onChange={e => updateLesson({name: e.target.value})}/>
+           : lesson.name
+          }
+        </span>
       </h2>
       <div className="LessonHeader__steps">
         {lesson.steps.map((step, index) => {
