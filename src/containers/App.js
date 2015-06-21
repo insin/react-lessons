e624c2c71aca789ef9804a7ffea7bad22de2e061
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-06-21 21:01:32 +0100

    Update default lesson

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,13 +5,28 @@ var {createRedux} = require('redux')
 var {Provider} = require('redux/react')
 var stores = require('../stores/index')
 
+var DEFAULT_TEXT = `## Lesson text
+
+Use Markdown for lesson text.
+
+The following variables are available for use when writing code:
+
+* \`React\`
+* \`output\` - the DOM node for the output area below
+
+JSX and all other ES6 and experimental ES7 features supported by Babel can be used in code.
+`
+
+var DEFAULT_CODE = `React.render(<div>Rendered content</div>, output)
+`
+
 var redux = createRedux(stores, {
   lessons: {
     editing: false,
     lessons: [{
-      name: 'Thinking In React',
+      name: 'Lesson',
       steps: [
-        {text: '## Start with a mock', code: '', solution: ''}
+        {text: DEFAULT_TEXT, code: DEFAULT_CODE}
       ]
     }],
     currentLessonIndex: 0,
