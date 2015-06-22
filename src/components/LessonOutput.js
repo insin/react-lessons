10e15ae7d6b509110e5566e9d4367f0199875e73
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-06-22 08:46:52 +0100

    Display error messages in output section

--- src/components/LessonOutput.js
+++ src/components/LessonOutput.js
@@ -4,24 +4,41 @@ var babel = require('babel-core/browser')
 require('./LessonOutput.css')
 
 var LessonOutput = React.createClass({
+  getInitialState() {
+    return {
+      errorMessage: ''
+    }
+  },
   componentDidMount() {
     this.executeCode()
   },
   componentWillReceiveProps(nextProps) {
-    if (nextProps.code !== this.props.code) {
+    if (this.props.code !== nextProps.code) {
       this.executeCode(nextProps)
     }
   },
   executeCode(props) {
     props = props || this.props
-    var code = babel.transform(props.code).code
-    /*eslint-disable no-new-func */
-    var func = new Function('React', 'output', code)
-    /*eslint-enable no-new-func */
-    func.call(this, React, React.findDOMNode(this.refs.output))
+    var output = React.findDOMNode(this.refs.output)
+    var errorMessage = ''
+    try {
+      var code = babel.transform(props.code).code
+      /*eslint-disable no-new-func */
+      var func = new Function('React', 'output', code)
+      /*eslint-enable no-new-func */
+      func.call(this, React, output)
+    }
+    catch (e) {
+      errorMessage = e.message
+      React.unmountComponentAtNode(output)
+    }
+    this.setState({errorMessage})
   },
   render() {
     return <div className="LessonOutput">
+      {this.state.errorMessage && <div className="LessonOutput__error">
+        {this.state.errorMessage}
+      </div>}
       <div ref="output"/>
     </div>
   }
