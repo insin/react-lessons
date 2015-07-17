196460e820471fe36c690d7198b95e06186bdc0d
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-07-17 02:09:10 +0100

    Use redux-action-utils

--- src/actions.js
+++ src/actions.js
@@ -1,20 +1,17 @@
+var {action} = require('redux-action-utils')
 var types = require('./types')
 
-var createAction = (type, ...props) =>
-  (...args) =>
-    props.reduce((action, prop, i) => (action[prop] = args[i], action), {type})
-
 module.exports = {
-  addLesson: createAction(types.ADD_LESSON),
-  addStep: createAction(types.ADD_STEP),
-  deleteLesson: createAction(types.DELETE_LESSON),
-  deleteStep: createAction(types.DELETE_STEP),
-  executeCode: createAction(types.EXECUTE_CODE, 'code'),
-  importLessons: createAction(types.IMPORT_LESSONS, 'imported'),
-  selectLesson: createAction(types.SELECT_LESSON, 'lessonIndex'),
-  selectStep: createAction(types.SELECT_STEP, 'stepIndex'),
-  toggleEditing: createAction(types.TOGGLE_EDITING, 'editing'),
-  updateCode: createAction(types.UPDATE_CODE, 'code'),
-  updateLesson: createAction(types.UPDATE_LESSON, 'update'),
-  updateStep: createAction(types.UPDATE_STEP, 'update')
+  addLesson: action(types.ADD_LESSON),
+  addStep: action(types.ADD_STEP),
+  deleteLesson: action(types.DELETE_LESSON),
+  deleteStep: action(types.DELETE_STEP),
+  executeCode: action(types.EXECUTE_CODE, 'code'),
+  importLessons: action(types.IMPORT_LESSONS, 'imported'),
+  selectLesson: action(types.SELECT_LESSON, 'lessonIndex'),
+  selectStep: action(types.SELECT_STEP, 'stepIndex'),
+  toggleEditing: action(types.TOGGLE_EDITING, 'editing'),
+  updateCode: action(types.UPDATE_CODE, 'code'),
+  updateLesson: action(types.UPDATE_LESSON, 'update'),
+  updateStep: action(types.UPDATE_STEP, 'update')
 }
